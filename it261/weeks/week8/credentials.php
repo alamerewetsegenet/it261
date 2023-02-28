4d03b41ae4e8401b0de4987fc1df0cdc69f0b86f
Etsegenet <114258837+alamerewetsegenet@users.noreply.github.com>
2023-02-27 21:36:11 -0800

    Update credentials.php

--- it261/weeks/week8/credentials.php
+++ it261/weeks/week8/credentials.php
@@ -3,7 +3,7 @@
 //school_pro_user - name of user
 
 
-define('DB_NAME', 'school_pro');
-define('DB_USER', 'school_pro_user');
-define('DB_PASSWORD', 'Yemanen23$');
-define('DB_HOST', 'mysql.etsegenet.com');
+define('DB_NAME', '');
+define('DB_USER', '');
+define('DB_PASSWORD', '');
+define('DB_HOST', '');
