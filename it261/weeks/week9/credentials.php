f46f885c0bef9f1d58a0b3ade4872fd6b05455d2
Etsegenet <114258837+alamerewetsegenet@users.noreply.github.com>
2023-03-04 00:23:26 -0800

    Add files via upload

---
+++ it261/weeks/week9/credentials.php
@@ -0,0 +1,9 @@
+<?php
+//school_pro- name of my database
+//school_pro_user - name of user
+
+
+define('DB_NAME', 'school_pro');
+define('DB_USER', 'school_pro_user');
+define('DB_PASSWORD', 'Yemanen23$');
+define('DB_HOST', 'mysql.etsegenet.com');
