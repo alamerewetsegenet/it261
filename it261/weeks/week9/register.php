569fa336497855943451d99dd181bb1aa842a75a
Etsegenet <114258837+alamerewetsegenet@users.noreply.github.com>
2023-03-13 13:38:19 -0700

    Update register.php

--- it261/weeks/week9/register.php
+++ it261/weeks/week9/register.php
@@ -1,10 +1,11 @@
 <?php
 include('server.php');
 //enentually, we will place our header include here!
-//include('./includes/header.php');
+include('./includes/header.php');
 
 ?>
-<h1>Register Today!</h1>
+<div id="wrapper">
+<h1 class="center">Register Today!</h1>
 
 <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ;?>" method="post">
 <fieldset>
@@ -27,12 +28,24 @@ include('server.php');
 <label>Password</label>
 <input type="password" name="password_1" value="<?php if(isset($_POST['password_1'])) echo htmlspecialchars($_POST['password_1']) ;?>">
 
-<label>Confirm your Password</label>
+<label>Confirm Password</label>
 <input type="password" name="password_2" value="<?php if(isset($_POST['password_2'])) echo htmlspecialchars($_POST['password_2']) ;?>">
-<!-- use the reg_user name below!!!---->
+<!--- use the reg_user name below!!!-->
 
 <button type="submit" name="reg_user" class="btn">Register</button>
 
 <button type="button" onclick="window.location.href='<?Php echo htmlspecialchars($_SERVER['PHP_SELF'])  ;?>' ">Reset</button>
+
+<?php
+include('errors.php');
+?>
+
 </fieldset>
 </form>
+
+<p class="center">Already have an account? <a href="login.php">Please login!</a></p>
+</div>
+<!---close wrapper div-->
+
+<?php
+include('./includes/footer.php');
