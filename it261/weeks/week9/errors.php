0ee09822aaadc5b048c21a4a29fb2d38e0049924
Etsegenet <114258837+alamerewetsegenet@users.noreply.github.com>
2023-03-13 13:41:01 -0700

    Add files via upload

---
+++ it261/weeks/week9/errors.php
@@ -0,0 +1,18 @@
+<?php
+
+//errors page which we will be using a for each loop
+
+//we are going to be counting errors 
+//if we have more than ZERO, not a good thing, we will need to echo our message.
+
+if(count($errors) > 0):?>
+<div class="errors">
+<?php foreach($errors as $error) : ?>
+<p>
+<?= $error ?>
+</p>  
+<?php endforeach; ?>
+</div>
+<!--end div error--->
+
+<?php endif; ?>
